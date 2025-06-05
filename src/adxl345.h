16551a9c40fb83a635313b0ba96771c74f83fcb2
unknown <tzcao@ucsc.edu>
2025-06-05 01:41:38 -0700

    wrote function prototypes

--- src/adxl345.h
+++ src/adxl345.h
@@ -10,18 +10,20 @@ We need functions to:
         x, y, z acceleration
 */
 
-#define I2C_ADDRESS 0x53    //device address
+#define I2C_ADDRESS 0x53    //i2c device address
+#define DEVID_ADDRESS 0x00  //device id (READ ONLY)
 #define POWER_CTL 0x2D      //power control address (for waking up sensor)
 #define DATA_FORMAT 0x31    //data format address (for setting range of data)
 
-void sensorInit() //initate sensor communication
-{
+void sensorInit(); //initate sensor communication 
     //load the sensor ID (check this to make sure we have connection)
+
+
+void setParams(uint8_t range);
     //Store sensor settings into registers
         //Range is D1 and D0 of 0x31 register:
             //2g -> value of 00, 4g -> 01, 8g -> 10, 16g -> 11
         //Make the sensor wakeup 0x08 -> 0x2D register
-}
 
 
 #endif
