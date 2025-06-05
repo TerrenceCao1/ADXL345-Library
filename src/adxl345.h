968e325a1c285052a44864870b556b9bfad111c9
unknown <tzcao@ucsc.edu>
2025-06-05 00:45:46 -0700

    initial commit

---
+++ src/adxl345.h
@@ -0,0 +1,14 @@
+#ifndef ADXL345_H_
+#define ADXL345_H_
+
+/*
+We need functions to:
+    Initiate communication between Arduino and Sensor
+    Set parameters inside the sensor's registers
+    Read data from I2C from sensor
+    Parse/interpret the data
+        x, y, z acceleration
+*/
+
+
+#endif
