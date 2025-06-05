f6b1c03862016f87b6cd7e74520c192281ca2170
unknown <tzcao@ucsc.edu>
2025-06-05 01:18:54 -0700

    pseudocode and defining registers

---
+++ src/adxl345.cpp
@@ -0,0 +1,3 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "adxl345.h"
--- src/adxl345.h
+++ src/adxl345.h
@@ -10,5 +10,18 @@ We need functions to:
         x, y, z acceleration
 */
 
+#define I2C_ADDRESS 0x53    //device address
+#define POWER_CTL 0x2D      //power control address (for waking up sensor)
+#define DATA_FORMAT 0x31    //data format address (for setting range of data)
+
+void sensorInit() //initate sensor communication
+{
+    //load the sensor ID (check this to make sure we have connection)
+    //Store sensor settings into registers
+        //Range is D1 and D0 of 0x31 register:
+            //2g -> value of 00, 4g -> 01, 8g -> 10, 16g -> 11
+        //Make the sensor wakeup 0x08 -> 0x2D register
+}
+
 
 #endif
